b8e4cf173159197ad9230cefeaa9a33b6483d347
jiaoshijie <jiao1751959040@gmail.com>
2021-08-27 15:33:51 +0800

    What: auto install packer.nvim

--- init.lua
+++ init.lua
@@ -1,5 +1,12 @@
 -- https://github.com/nanotee/nvim-lua-guide
 
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
+
 require('packer').startup(function(use)
 
   -- {{{ Installer
