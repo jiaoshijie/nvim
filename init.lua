776dc6e954aae0caa241528d63e73037a32114e5
jiaoshijie <jiao1751959040@gmail.com>
2021-11-11 22:59:32 +0800

    What: added filetype.nvim plugin

--- init.lua
+++ init.lua
@@ -167,4 +167,12 @@ require('packer').startup(function(use)
     end,
   }
 
+  use {
+    'nathom/filetype.nvim',
+    config = function()
+      -- Do not source the default filetype.vim
+      vim.g.did_load_filetypes = 1
+    end
+  }
+
 end)
