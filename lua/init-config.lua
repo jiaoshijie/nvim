d119bab6609de56285ebc287494c3400895dc993
jiaoshijie <jiao1751959040@gmail.com>
2022-05-11 19:34:21 +0800

    What: disable load node support

--- lua/init-config.lua
+++ lua/init-config.lua
@@ -7,6 +7,7 @@ vim.g.maplocalleader = ','
 
 
 vim.g.loaded_python_provider = 0  -- To disable Python2 support
+vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0  -- To disable Perl support
 vim.g.loaded_ruby_provider = 0  -- To disable Ruby support
 -- vim.g.python_host_prog = '/usr/bin/python2'
