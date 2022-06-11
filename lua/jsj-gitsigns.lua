12c206d8a3d9849055dc44e210bb2a41711452ed
jiaoshijie <jiao1751959040@gmail.com>
2022-06-11 15:18:19 +0800

    What: don't use codeAction

--- lua/jsj-gitsigns.lua
+++ lua/jsj-gitsigns.lua
@@ -26,16 +26,4 @@ require("gitsigns").setup({
   status_formatter = nil, -- Use default
 })
 
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
-map("n", "<leader>ga", "<cmd>Gitsigns stage_hunk<cr>", opts)
-map("n", "<leader>gn", "<cmd>Gitsigns next_hunk<cr>", opts)
-map("n", "<leader>gp", "<cmd>Gitsigns prev_hunk<cr>", opts)
-map("n", "<leader>gs", "<cmd>Gitsigns select_hunk<cr>", opts)
-map("n", "<leader>gua", "<cmd>Gitsigns undo_stage_hunk<cr>", opts)
-map("n", "<leader>gP", "<cmd>Gitsigns preview_hunk<cr>", opts)
-map("n", "<leader>gB", "<cmd>Gitsigns stage_buffer<cr>", opts)
-map("n", "<leader>gub", "<cmd>Gitsigns reset_buffer_index<cr>", opts)
-map("n", "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", opts)
-map("n", "<leader>gh", "<cmd>Gitsigns toggle_linehl<cr>", opts)
+vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", { noremap = true, silent = true })
