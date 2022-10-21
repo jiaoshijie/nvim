ba5ce4690bc1fcff45f1dd1e5fc6660573a75c67
jiaoshijie <jiao1751959040@gmail.com>
2022-10-21 10:43:21 +0800

    update neovide config

--- lua/init-neovide.lua
+++ lua/init-neovide.lua
@@ -1,12 +1,28 @@
 -- https://neovide.dev/
-vim.opt.guifont = { "Fira_Code:h13", "JetBrains_Mono:h13", "Hack:h13" }
+vim.opt.guifont = { "JetBrains_Mono:h13", "Hack:h13" }
 vim.opt.guicursor = {
   "n-v-c-sm:block-Cursor", "i-ci-ve:ver15-Cursor", "r-cr-o:hor20-Cursor"
 }
 
 if vim.g.neovide then
-  vim.g.neovide_transparency = 0.95
+  vim.g.neovide_transparency = 0.8
   vim.g.neovide_confirm_quit = true
-  vim.g.neovide_fullscreen = false
-  vim.g.neovide_cursor_vfx_mode = "sonicboom"
+  vim.g.neovide_cursor_vfx_mode = "ripple"
+
+  vim.g.neovide_scale_factor = 1.0
+  local change_font_size = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<C-=>", function() change_font_size(1.25) end,
+    { noremap = true })
+  vim.keymap.set("n", "<C-->", function() change_font_size(1/1.25) end,
+    { noremap = true })
+
+  vim.keymap.set("n", "<leader>tt", function()
+    if vim.g.neovide_transparency == 1.0 then
+      vim.g.neovide_transparency = 0.8
+    else
+      vim.g.neovide_transparency = 1.0
+    end
+  end, { noremap = true })
 end
