e5b8a8ad278890197a1006a679cc5c5a0b3f89f8
jiaoshijie <jiao1751959040@gmail.com>
2023-08-07 00:14:55 +0800

    remove after/syntax/markdown.lua

--- after/syntax/markdown.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd.syntax([[region markdownLinkText matchgroup=markdownLinkTextDelimiter]] ..
-  [[ start="!\=\[\%(\%(\_[^][]\|\[\_[^][]*\]\)*]\%( \=[[(]\)\)\@=" end="\]\%( \=[[(]\)\@="]] ..
-  [[ nextgroup=markdownLink,markdownId skipwhite]] ..
-  [[ contains=@markdownInline,markdownLineStart concealends]])
-
-vim.cmd.syntax([[region markdownLink matchgroup=markdownLinkDelimiter]] ..
-  [[ start="(" end=")" contains=markdownUrl keepend contained conceal]])
