480867a3dd40a2f8bb957dcdff0fac8e8be48010
jiaoshijie <jiao1751959040@gmail.com>
2024-05-26 17:13:36 +0800

    add a function to insert table in markdown file

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,3 +1,6 @@
+local vf = vim.fn
+local api = vim.api
+
 vim.opt_local.conceallevel = 3
 vim.opt_local.concealcursor = ""
 vim.g.markdown_syntax_conceal = 1
@@ -26,12 +29,21 @@ local mdlist = {
   { k = "4",  e = "#### <CR><CR><++><Esc>2kA" },
   { k = "5",  e = "##### <CR><CR><++><Esc>2kA" },
   { k = "6",  e = "###### <CR><CR><++><Esc>2kA" },
-  { k = ",",  e = "<br>" },
-  { k = "z",  e = "<font color=><++></font><Esc>F>;i" },
-  { k = "t2", e = [[|<++>|<++>|<cr>|----|----|<cr><Esc>2kyyj2p2ki<Esc>]] },
-  { k = "t3", e = [[|<++>|<++>|<++>|<cr>|----|----|----|<cr><Esc>2kyyj2p2ki<Esc>]] },
-  { k = "t4", e = [[|<++>|<++>|<++>|<++>|<cr>|----|----|----|----|<cr><Esc>2kyyj2p2ki<Esc>]] },
 }
+
+local insert_table = function()
+  local cols = tonumber(vf.input("Enter the number of columns > "))
+  vim.cmd("echo '' | redraw")  -- clear the cmd line
+
+  if cols == nil then
+    api.nvim_err_writeln("The input must be a number!!!")
+    return
+  end
+
+  local body = '|' .. string.rep('<++>|', cols)
+  local sep  = '|' .. string.rep('----|', cols)
+  return body .. '<cr>' .. sep .. '<cr><Esc>2kyyj2p2ki<Esc>'
+end
 
 for _, i in ipairs(mdlist) do
   local m = i.m == nil and "i" or i.m
@@ -42,3 +54,5 @@ for _, i in ipairs(mdlist) do
   end
   vim.keymap.set(m, k, i.e, opts)
 end
+
+vim.keymap.set('i', '<localleader>t', insert_table, { noremap = true, buffer = true, expr = true })
