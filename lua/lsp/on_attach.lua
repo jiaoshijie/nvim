d2ae085c57ffd8a40d2a417730e99e64b1a371b7
jiaoshijie <jiao1751959040@gmail.com>
2024-06-13 15:42:26 +0800

    Enable nvim builtin completion on nightly nvim version

--- lua/lsp/on_attach.lua
+++ lua/lsp/on_attach.lua
@@ -1,38 +1,47 @@
 local symbols_com = require("lsp.component").on_attach
+local keymap = vim.keymap.set
 
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  -- Completion
+  if client.supports_method('textDocument/completion') then
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    vim.lsp.completion.enable(true, client.id, bufnr, {autotrigger = false})
+    keymap({'i', 's'}, '<C-l>', function()
+      if vim.snippet.active({direction = 1}) then
+        vim.snippet.jump(1)
+      end
+    end, opts)
+    keymap({'i', 's'}, '<C-b>', function()
+      if vim.snippet.active({direction = -1}) then
+        vim.snippet.jump(-1)
+      end
+    end, opts)
+  end
 
   -- Mappings.
-  local opts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-  vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
-  vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover, opts)
-  vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
+  keymap("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+  keymap("n", "gd", vim.lsp.buf.definition, opts)
+  keymap("n", "gD", vim.lsp.buf.declaration, opts)
+  keymap("n", "gr", vim.lsp.buf.references, opts)
+  keymap("n", "gi", vim.lsp.buf.implementation, opts)
+  keymap("n", "<leader>cr", vim.lsp.buf.rename, opts)
+  keymap("n", "<C-k>", vim.lsp.buf.hover, opts)
+  keymap("i", "<C-k>", vim.lsp.buf.signature_help, opts)
 
-  vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-  vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-  vim.keymap.set("n", "<leader>wl", function()
+  keymap("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+  keymap("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+  keymap("n", "<leader>wl", function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, opts)
 
   local lsp_on_attach = vim.api.nvim_create_augroup("jsj_lsp_on_attach_" .. bufnr, { clear = true })
 
   if client.server_capabilities.documentFormattingProvider then
-    vim.keymap.set("n", "<leader>=", function()
+    keymap("n", "<leader>=", function()
       vim.lsp.buf.format({ async = true })
     end, opts)
-    -- NOTE: don't auto format on save.
-    -- vim.api.nvim_create_autocmd("BufWritePre", {
-    --   group = lsp_on_attach,
-    --   buffer = bufnr,
-    --   callback = function() vim.lsp.buf.format({ async = false }) end,
-    -- })
   end
 
   if client.server_capabilities.documentHighlightProvider then
@@ -49,7 +58,7 @@ local on_attach = function(client, bufnr)
   end
 
   if client.server_capabilities.codeActionProvider then
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
   end
 
   if client.server_capabilities.documentSymbolProvider then
