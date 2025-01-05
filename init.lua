a40b39b6656ae27bdd7155927496dbe3ea18398f
jiaoshijie <jiao1751959040@gmail.com>
2025-01-05 12:35:03 +0800

    add gnu global tags system plugin

--- init.lua
+++ init.lua
@@ -255,6 +255,20 @@ require("pkg")
 
 -------------------------------------------------------------------------------
 
+-- GNU `global` tool
+-- https://www.gnu.org/software/global/globaldoc_toc.html#Vim-editor
+local jsj_gnu_global_plugin_path = nil
+if vim.fn.filereadable("/usr/share/gtags/gtags.vim") == 1 then  -- for void linux
+    jsj_gnu_global_plugin_path = "/usr/share/gtags/gtags.vim"
+elseif vim.fn.filereadable("/usr/share/vim/addons/plugin/gtags.vim") == 1 then  -- for debian-based
+    jsj_gnu_global_plugin_path = "/usr/share/vim/addons/plugin/gtags.vim"
+end
+if jsj_gnu_global_plugin_path ~= nil then
+    vim.cmd('so ' .. jsj_gnu_global_plugin_path)
+end
+
+-------------------------------------------------------------------------------
+
 if vim.fn.filereadable(vim.fn.expand('~/.nvimrc')) == 1 then
     vim.cmd('so ~/.nvimrc')
 end
