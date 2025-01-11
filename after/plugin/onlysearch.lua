358f90d2fe9436bc7e35695d22f82d6823d0d37d
jiaoshijie <jiao1751959040@gmail.com>
2025-01-11 22:17:22 +0800

    updata onlysearch config and submodule

--- after/plugin/onlysearch.lua
+++ after/plugin/onlysearch.lua
@@ -5,18 +5,45 @@ if not found then
 end
 
 local engine = 'rg'
+local args = {}
+local complete = {
+    {
+        word = '-w',
+        kind = ' [W] Match Whole Word',
+    },
+    {
+        word = '-i',
+        kind = ' [I] Ignore Case'
+    },
+    {
+        word = '-.',
+        kind = ' [A] Search All Files'
+    },
+}
 
 if vim.fn.executable(engine) ~= 1 then
     engine = 'grep'
+    complete = {
+        {
+            word = '-w',
+            kind = ' [W] Match Whole Word',
+        },
+        {
+            word = '-i',
+            kind = ' [I] Ignore Case'
+        },
+    }
 end
 
 onlysearch.setup({
     engine = engine,
-    engine_config = {},
-    open_cmd = "vnew",
+    engine_config = {
+        args = args,
+        complete = complete,
+    },
+    open_cmd = 'vnew',
 })
 
--- vim.api.nvim_create_user_command("Os", function()
---     onlysearch.toggle()
--- end, { nargs = 0 })
-vim.keymap.set('n', 'S', onlysearch.toggle, { silent = true, noremap = true })
+vim.api.nvim_create_user_command("Os", function()
+    onlysearch.toggle()
+end, { nargs = 0 })
