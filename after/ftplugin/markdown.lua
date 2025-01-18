56c2fb5502f689ea739aa23f08f59eb548fdaba0
jiaoshijie <jiao1751959040@gmail.com>
2025-01-18 22:56:27 +0800

    make markdown file indentation to 2, and update onlysearch submodule

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,6 +3,9 @@ local api = vim.api
 
 vim.opt_local.conceallevel = 3
 vim.opt_local.concealcursor = ""
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
 
 local l = "<localleader>"
 local mdlist = {
