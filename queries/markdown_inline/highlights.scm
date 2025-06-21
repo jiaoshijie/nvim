dd51e2e2a634154213459be5bf45743eb5b3072e
jiaoshijie <jiao1751959040@gmail.com>
2025-06-21 11:46:31 +0800

    update treesitter local queries

--- queries/markdown_inline/highlights.scm
+++ queries/markdown_inline/highlights.scm
@@ -39,6 +39,21 @@
   ] @markup.link
   (#set! conceal ""))
 
+[
+  (link_label)
+  (link_text)
+  (link_title)
+  (image_description)
+] @markup.link.label
+
+((inline_link
+  (link_destination) @_url) @_label
+  (#set! @_label url @_url))
+
+((image
+  (link_destination) @_url) @_label
+  (#set! @_label url @_url))
+
 ; Conceal image links
 (image
   [
@@ -79,19 +94,19 @@
 [
   (link_destination)
   (uri_autolink)
+  (email_autolink)
 ] @markup.link.url @nospell
 
-[
-  (link_label)
-  (link_text)
-  (link_title)
-  (image_description)
-] @markup.link.label
+((uri_autolink) @_url
+  (#offset! @_url 0 1 0 -1)
+  (#set! @_url url @_url))
+
+(entity_reference) @nospell
 
 ; Replace common HTML entities.
 ((entity_reference) @character.special
   (#eq? @character.special "&nbsp;")
-  (#set! conceal ""))
+  (#set! conceal " "))
 
 ((entity_reference) @character.special
   (#eq? @character.special "&lt;")
