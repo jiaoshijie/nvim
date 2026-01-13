f29f82a6958dbf5e58ecb6e383043457f3ddb274
jiaoshijie <jiao1751959040@gmail.com>
2026-01-13 23:02:59 +0800

    using the default undotree config

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -4,26 +4,4 @@ if not found then
     return
 end
 
-undotree.setup({
-    float_diff = true,      -- set this `true` will disable layout option
-    layout = "left_bottom", -- "left_bottom", "left_left_bottom"
-    position = "left",
-    ignore_filetype = { 'Undotree', 'UndotreeDiff', 'qf', 'query' },
-    window = {
-        winblend = 0,
-        border = "rounded",
-    },
-    keymaps = {
-        ['j'] = "move_next",
-        ['k'] = "move_prev",
-        ['J'] = "move_change_next",
-        ['K'] = "move_change_prev",
-        ['<cr>'] = "action_enter",
-        ['p'] = "enter_diffbuf",
-        ['q'] = "quit",
-    },
-})
-
-vim.api.nvim_create_user_command('Undotree', function()
-    undotree.toggle()
-end, { nargs = 0 })
+vim.api.nvim_create_user_command('Undotree', undotree.toggle, { nargs = 0 })
