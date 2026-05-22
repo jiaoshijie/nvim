600255b130cfef74c6e476521a95afdd72528398
jiaoshijie <jsj_151@foxmail.com>
2026-05-22 16:25:54 +0800

    fix potential bug on the use of vim.fn.getenv

--- init.lua
+++ init.lua
@@ -105,7 +105,7 @@ o.switchbuf = "uselast"
 o.backup = false
 o.swapfile = false
 o.undofile = true
-o.undodir = string.format("/tmp/%s_neovim_u/undodir//", vim.fn.getenv("USER") or "jsj")
+o.undodir = string.format("/tmp/%s_neovim_u/undodir//", kit.get_username())
 
 -- NOTE: colorscheme
 o.signcolumn = "no"
