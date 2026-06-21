02862c5e35a1fb803c935077327dea9a2d67b501
jiaoshijie <jiao1751959040@gmail.com>
2026-06-21 17:01:30 +0800

    add MdTab* commands for markdown file

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,6 @@
-local vf = vim.fn
 local kit = require('kit')
+local map = vim.keymap.set
+local command = vim.api.nvim_buf_create_user_command
 
 vim.opt_local.conceallevel = 3
 vim.opt_local.concealcursor = ""
@@ -7,41 +8,56 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 
-local l = "<localleader>"
-local mdlist = {
-    {
-        m = "n",
-        slient = true,
-        k = "f",
-        e =
-        '/<++><cr>:nohl<cr>"_c4l'
-    },
-    { k = "f",  e = '<Esc>/<++><cr>:nohl<cr>"_c4l' },
-}
+map('n', '<localleader>f', '/<++><cr>:nohl<cr>"_c4l', { noremap = true, buffer = true, silent = true })
+map('i', '<localleader>f', '<Esc>/<++><cr>:nohl<cr>"_c4l', { noremap = true, buffer = true })
 
-local insert_table = function()
-    local cols = tonumber(vf.input("Enter the number of columns > "))
-    vim.cmd("echo '' | redraw")  -- clear the cmd line
+local table_insert = function(args)
+    local cols = tonumber(args.args)
 
     if cols == nil then
-        kit.echo_err_msg("The input must be a number!!!")
+        kit.echo_err_msg("The column must be a number!!!")
         return
     end
 
-    local body = '|' .. string.rep('<++>|', cols)
-    local sep  = '|' .. string.rep('----|', cols)
-    return body .. '<cr>' .. sep .. '<cr><Esc>2kyyj2p2ki<Esc>'
-end
-
-for _, i in ipairs(mdlist) do
-    local m = i.m == nil and "i" or i.m
-    local k = l .. i.k
-    local opts = { noremap = true, buffer = true }
-    if i.slient then
-        opts.silent = true
+    local curpos = vim.api.nvim_win_get_cursor(0)
+    if vim.fn.getline(curpos[1]) ~= "" then
+        kit.echo_err_msg("The cursor must be on an empty line!!!")
+        return
     end
-    vim.keymap.set(m, k, i.e, opts)
+
+    local header = '|' .. string.rep('<++>|', cols)
+    local sep  = '|' .. string.rep('----|', cols)
+    local body = header
+
+    vim.api.nvim_buf_set_lines(0, curpos[1] - 1, curpos[1], false, { header, sep, body, body })
 end
 
-vim.keymap.set('i', '<localleader>t', insert_table, { noremap = true, buffer = true, expr = true })
+local table_align = function(spec)
+    if vim.fn.exists(':Tabularize') == 0 then
+        kit.echo_err_msg("`Tabularize` not available!!!")
+        return
+    end
+
+    local line = vim.fn.getline('.')
+    local cols = 0
+
+    for i = 1,#line do
+        if line:sub(i, i) == '|' then
+            cols = cols + 1
+        end
+    end
+
+    if cols == 0 or cols == 1 then
+        kit.echo_err_msg("The cursor must be on a line in a table!!!")
+        return
+    end
+
+    vim.cmd("Tabularize /|/c0" .. string.rep(spec, cols - 1) .. "c0")
+end
+
+command(0, "MdTabInsert", table_insert, { nargs = 1 })
+command(0, "MdTabAlign", function() table_align("c1l1") end, { nargs = 0 })
+command(0, "MdTabAlignRight", function() table_align("c1r1") end, { nargs = 0 })
+command(0, "MdTabAlignCenter", function() table_align("c1c1") end, { nargs = 0 })
+
 vim.treesitter.start()
